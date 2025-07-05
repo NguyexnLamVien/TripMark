1ae43ce9ba4d4afc06a83b30432000fc059dead7
NguyexnLamVien <nlv.clone@gmail.com>
2025-07-05 17:13:42 +0700

    finish login

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,24 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+import { useFakeAuth } from "../context/FakeAuthContext";
 
 export default function Login() {
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("");
   let navigate = useNavigate();
+  const { login, isAuthenticated } = useFakeAuth();
 
   function handleSubmit(e) {
     e.preventDefault();
-    if (email === "jack@example.com" && password === "1234") {
-      navigate("/app");
-    } else {
-      alert("Invalid credentials");
-      navigate("/login");
-    }
+    login(email, password);
   }
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/app", { replace: true });
+    }
+  }, [isAuthenticated, navigate]);
 
   return (
     <form
-      action="submit"
       onSubmit={(e) => handleSubmit(e)}
       className="bg-[#42484d] rounded-2xl w-[40%] flex flex-col gap-4  mt-32 mx-auto px-8 py-12"
     >
