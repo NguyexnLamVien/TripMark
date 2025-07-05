b0dcb961980121a21404a29445ff87b21b5e9d1f
NguyexnLamVien <nlv.clone@gmail.com>
2025-07-05 17:16:28 +0700

    xoá code dư

--- src/page/MapLayout.jsx
+++ src/page/MapLayout.jsx
@@ -1,23 +1,10 @@
 import { Outlet, useNavigate } from "react-router";
 import SideBar from "../components/SideBar";
 import Map from "../components/Map";
-import { useEffect } from "react";
-import { useFakeAuth } from "../context/FakeAuthContext";
+
 import User from "../components/user";
 
 export default function MapLayout() {
-  let navigate = useNavigate();
-
-  const { isAuthenticated } = useFakeAuth();
-
-  // useEffect(function () {
-  //   if (isAuthenticated !== true) {
-  //     navigate("/login");
-  //   }
-  // }, []);
-
-  console.log("isAuthenticated", isAuthenticated);
-
   return (
     <div className="flex justify-center m-[2.5rem] h-[calc(100vh-5rem)] px-2.5 py-5 relative">
       <div className="sidebar">
